39258b2d7bd543fb72381f5e9bd38fe0fcca6342
rippunpirdasari27 <rippunsari27@sma.belajar.id>
2025-03-28 17:55:34 +0700

    Add files via upload

---
+++ postes/src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) { 
+        Employee emp = new Employee("Alice", 25, 5000.0); 
+        emp.display();   
+        emp.showSalary(); // Menampilkan gaji
+    }
+}
